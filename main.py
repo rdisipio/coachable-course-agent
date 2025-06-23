d7b6a5fa586bac806d6ff5968bd28746bbbb55ab
rdisipio <riccardo.disipio@dayforce.com>
2025-06-23 20:33:59 +0000

    wip

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from coachable_course_agent.memory_store import load_user_profile, update_user_p
 from coachable_course_agent.feedback_processor import process_feedback
 from coachable_course_agent.vector_store import initialize_chroma, add_courses_to_chroma, query_similar_courses
 from coachable_course_agent.agent_runner import create_course_agent
+from coachable_course_agent.recommendation_prompt import base_prompt
 
 # Load course catalog and ESCO skills
 courses = load_courses("data/course_catalog_esco.json")
@@ -22,10 +23,28 @@ user_profile = load_user_profile(user_id)
 agent = create_course_agent()
 
 # Run the agent to get recommendation response
-response = agent.run({
-    "chroma": chroma_collection,
-    "profile": user_profile
-})
+instructions = base_prompt.format(
+    goal=user_profile["goal"],
+    known_skills=", ".join(user_profile["known_skills"]),
+    missing_skills=", ".join(user_profile["missing_skills"]),
+    format=", ".join(user_profile["preferences"]["format"]),
+    style=", ".join(user_profile["preferences"]["style"]),
+    avoid_styles=", ".join(user_profile["preferences"].get("avoid_styles", [])),
+    feedback_log=json.dumps(user_profile.get("feedback_log", [])[-3:], indent=2)
+)
+
+input_payload = {
+    "goal": user_profile["goal"],
+    "known_skills": ", ".join(user_profile["known_skills"]),
+    "missing_skills": ", ".join(user_profile["missing_skills"]),
+    "format": ", ".join(user_profile["preferences"]["format"]),
+    "style": ", ".join(user_profile["preferences"]["style"]),
+    "avoid_styles": ", ".join(user_profile["preferences"].get("avoid_styles", [])),
+    "feedback_log": json.dumps(user_profile.get("feedback_log", [])[-3:], indent=2),
+    "instruction": instructions,
+}
+
+response = agent.run(input_payload)
 
 # Parse response (assume it's valid JSON for now)
 recommendations = json.loads(response)
