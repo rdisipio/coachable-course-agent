43245c7b190e4b745ccc4d270713cc97fa1fffbb
rdisipio <riccardo.disipio@dayforce.com>
2025-06-25 15:23:25 +0000

    wip

--- scripts/load_courses.py
+++ scripts/load_courses.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from tqdm import tqdm
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
